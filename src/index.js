715bb8b84ab814d53b3441dc176fe0c61847d31f
Hande Shraddha Suresh <95674349+hande2003@users.noreply.github.com>
2023-06-28 18:31:12 +0530

    added module.exports in index.js file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 let date = require("./router/date");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.post('/api', (req, res)=>{
-  res.json({unix: new Date().valueOf(), utc: new Date().toUTCString()});
+app.post("/api", (req, res) => {
+  res.json({ unix: new Date().valueOf(), utc: new Date().toUTCString() });
 });
 
-app.use('/api', date);
+app.use("/api", date);
 
-app.get('/', (req, res)=>{res.sendFile(process.cwd() + "/views/index.html")});
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
-app.use('/public', express.static(process.cwd() + "/public"));
+app.use("/public", express.static(process.cwd() + "/public"));
 
 let PORT = process.env.PORT || 3000;
-app.listen(PORT, () => (console.log(`Node is listening on port ${PORT}...`)));
+app.listen(PORT, () => console.log(`Node is listening on port ${PORT}...`));
+
+module.exports = app;
